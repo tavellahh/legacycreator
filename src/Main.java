7efa84615a737b99603c46aa2601324ad536207e
Hernan Hugo Tavella <tavellahh@gmail.com>
2023-07-10 19:42:57 -0300

    updates

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-
-
 import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Main {
@@ -14,83 +14,46 @@ public class Main {
 
         Main app = new Main();
 
-        //String fileName = "database.properties";
         String fileName = "hht.txt";
 
-        System.out.println("getResourceAsStream : " + fileName);
-        InputStream is = app.getFileFromResourceAsStream(fileName);
-        printInputStream(is);
-
         System.out.println("\ngetResource : " + fileName);
-        
-        
-        
         File file = app.getFileFromResource(fileName);
         printFile(file);
 
+        // Buscar y reemplazar texto en el archivo
+        String searchText = "hola";
+        String replaceText = "Replacement";
+        String destinationPath = "C:\\Users\\Hernan\\destination.txt"; // Ruta de destino del archivo modificado
+
+        app.searchAndReplaceInFile(file, searchText, replaceText, destinationPath);
     }
 
-    // get a file from the resources folder
-    // works everywhere, IDEA, unit test and JAR file.
-    private InputStream getFileFromResourceAsStream(String fileName) {
-
-        // The class loader that loaded the class
-        ClassLoader classLoader = getClass().getClassLoader();
-        InputStream inputStream = classLoader.getResourceAsStream(fileName);
-        
-        // the stream holding the file content
-        if (inputStream == null) {
-            throw new IllegalArgumentException("file not found! " + fileName);
-        } else {
-            return inputStream;
-        }
-
-    }
-
-    /*
-        The resource URL is not working in the JAR
-        If we try to access a file that is inside a JAR,
-        It throws NoSuchFileException (linux), InvalidPathException (Windows)
-
-        Resource URL Sample: file:java-io.jar!/json/file1.json
-     */
-    private File getFileFromResource(String fileName) throws URISyntaxException{
-
+ 
+    private File getFileFromResource(String fileName) throws URISyntaxException {
         ClassLoader classLoader = getClass().getClassLoader();
         URL resource = classLoader.getResource(fileName);
+
         if (resource == null) {
-            throw new IllegalArgumentException("file not found! " + fileName);
+            throw new IllegalArgumentException("File not found! " + fileName);
         } else {
-
-            // failed if files have whitespaces or special characters
-            //return new File(resource.getFile());
-
             return new File(resource.toURI());
         }
-
     }
 
-    // print input stream
     private static void printInputStream(InputStream is) {
-
-        try (InputStreamReader streamReader =
-                    new InputStreamReader(is, StandardCharsets.UTF_8);
+        try (InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
              BufferedReader reader = new BufferedReader(streamReader)) {
 
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    // print a file
     private static void printFile(File file) {
-
         List<String> lines;
         try {
             lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
@@ -98,7 +61,25 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
+    private void searchAndReplaceInFile(File file, String searchText, String replaceText, String destinationPath) {
+        try {
+            List<String> lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+
+            for (int i = 0; i < lines.size(); i++) {
+                String line = lines.get(i);
+                if (line.contains(searchText)) {
+                    line = line.replace(searchText, replaceText);
+                    lines.set(i, line);
+                }
+            }
+
+            Path destinationFilePath = Paths.get(destinationPath);
+            Files.write(destinationFilePath, lines, StandardCharsets.UTF_8);
+            System.out.println("File saved to: " + destinationPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
